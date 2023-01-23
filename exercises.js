a35bfea905066185a7600d2985412610f08fe5e7
RichardCastillo1226 <castillo.richard1226@gmail.com>
2023-01-22 22:05:43 -0500

    tests added

--- exercises.js
+++ exercises.js
@@ -1,17 +1,34 @@
-function octalToDecimal() {
-  //Write code here
+function octalToDecimal(string) {
+  return string.split("").reduce((prev, curr, i) => {
+    return prev + Math.pow(8, string.length - 1 - i) * curr
+  }, 0)
 }
 
-function anagram() {
-  //Write code here
+function anagram(string,arr) {
+  const result = []
+  const cloneStr = string.split("").sort().join("")
+  for (let eles of arr) {
+    if (cloneStr.length === eles.length) {
+      const sort = eles.split("").sort().join("")
+      console.log(sort)
+      if (cloneStr === sort) result.push(eles)
+    }
+  }
+  return result
 }
 
-function triangle() {
-  //Write code here
+function triangle(s1,s2,s3) {
+  const triangleType = [s1,s2,s3]
+  return triangleType.reduce((a,b) => a + b,0) !== 180 || triangleType.some(n => n === 0) ? "invalid" : triangleType.every(n => n < 90) ? "acute" : triangleType.some(n => n > 90) ? "obtuse" : "right"
 }
 
-function fridayThe13ths() {
-  //Write code here
+function fridayThe13ths(date) {
+  let count = 0
+  for(let m = 0; m < 12;m++){
+    let jason = new Date(date, m, 13)
+    if(jason.getDay() === 5) count++
+  }
+  return count
 }
 
 module.exports = {
